f050fc5da79cd9ab0c3540f24ea47481a3750baa
Kristian Bak <blaaman@gmail.com>
2014-02-22 18:05:46 +0100

    Added statemachine and singeton from NGJ

---
+++ Assets/NGJ/SingletonPattern/Scripts/AudioController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioController : MonoBehaviour
+{
+    #region Singleton Pattern 
+    private static AudioController _instance = null;
+    public static AudioController Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = Instantiate(Resources.Load("Sound/AudioController", typeof(AudioController))) as AudioController;
+                _instance.Init();
+                return _instance;
+            }
+            else if (_instance != null)
+            {
+                return _instance;
+            }
+            return null;
+        }
+    }
+    
+    void Awake()
+    {
+        Debug.Log("Awake");
+        if(_instance != null && _instance != this)
+        {
+            DestroyImmediate(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+    #endregion
+
+    public AudioClip pickUpCoin;
+    public AudioSource mainAudioSource;
+
+    void Init()
+    {
+        Debug.Log("Init");
+    }
+
+    public void PlaySound()
+    {
+        Debug.Log("Play Sound");
+        mainAudioSource.clip = pickUpCoin;
+        mainAudioSource.Play();        
+    }
+
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+}
